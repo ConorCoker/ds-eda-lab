4d28969edb439b9d7422ba2b2b18a03904d6224a
Conor Coker <conorcoker@gmail.com>
2025-04-03 14:33:44 +0100

    Started Fan out for S3 image upload.

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -5,6 +5,8 @@ import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3n from "aws-cdk-lib/aws-s3-notifications";
 import * as events from "aws-cdk-lib/aws-lambda-event-sources";
 import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as sns from "aws-cdk-lib/aws-sns";
+import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
 import { Construct } from "constructs";
 
 export class EDAAppStack extends cdk.Stack {
@@ -21,9 +23,22 @@ export class EDAAppStack extends cdk.Stack {
       value: imagesBucket.bucketName,
     });
 
-    const queue = new sqs.Queue(this, "img-created-queue", {
-      receiveMessageWaitTime: cdk.Duration.seconds(5),
+    const imageProcessQueue = new sqs.Queue(this, "img-created-queue", {
+      receiveMessageWaitTime: cdk.Duration.seconds(10),
     });
+
+    const newImageTopic = new sns.Topic(this, "NewImageTopic", {
+      displayName: "New Image topic",
+    });
+
+    imagesBucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new s3n.SnsDestination(newImageTopic)
+    );
+
+    newImageTopic.addSubscription(
+      new subs.SqsSubscription(imageProcessQueue)
+    );
 
     const processImageFn = new lambdanode.NodejsFunction(this, "ProcessImageFn", {
       runtime: lambda.Runtime.NODEJS_22_X,
@@ -32,12 +47,7 @@ export class EDAAppStack extends cdk.Stack {
       memorySize: 128,
     });
 
-    imagesBucket.addEventNotification(
-      s3.EventType.OBJECT_CREATED,
-      new s3n.SqsDestination(queue)
-    );
-
-    const newImageEventSource = new events.SqsEventSource(queue, {
+    const newImageEventSource = new events.SqsEventSource(imageProcessQueue, {
       batchSize: 5,
       maxBatchingWindow: cdk.Duration.seconds(5),
     });
@@ -47,7 +57,8 @@ export class EDAAppStack extends cdk.Stack {
     imagesBucket.grantRead(processImageFn);
 
     new cdk.CfnOutput(this, "queueName", {
-      value: queue.queueName,
+      value: imageProcessQueue.queueName,
     });
   }
 }
+
